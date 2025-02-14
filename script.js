3e028162e7fbdca796498081f686393fdb6a0b31
amandastekerhofa <amandastekerhofa@gmail.com>
2025-02-14 10:25:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+alert("Hello world");
+
+console.log("Hello world");
+*/
+
+/*
+let x = 5;
+let y = 5;
+
+if(x==y)
+{
+  console.log("x ir vienāds ar y");
+}
+*/
+
+
+let number = prompt("Enter number:");
+if(number > 0)
+{
+    console.log(number, "ir pozitīvs");
+}
+if(number < 0)
+{
+    console.log(number, "ir negatīvs");
+}
+if(number == 0)
+{
+    console.log(number, "ir 0")
+}
