3b2fb0f28f026be21b5e0553d7f2be3fa36bef6d
amandastekerhofa <amandastekerhofa@gmail.com>
2025-03-21 09:51:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,38 @@
 
-let a;parseFloat(prompt("Enter variable a"));
-let b;parseFloat(;prompt("Enter variable b"));
-//console.log(typeof(a)); // izvada mainīgo a t
-//console.log(typeof(b));
-let result;
-let operators = prompt("Enter operator: ");
+let a = 10;  
+let b = 5;  
 
-switch(operators)
-{
-case"+"
-    result = a + b:
-break;
-case"-"
-    result = a - b:
-break;
-case"*"
-    result = a * b:
-break;
-case"/"
-    result = a / b:
-break;
-case"^"
-    result = a ^ b:
-break;
-case"%"
-    result = a % b:
-break;
-case"!"
-    console.log(!)
-break;
+console.log("Summas rezultāts:", a + b);       
+console.log("Starpības rezultāts:", a - b);   
+console.log("Reizinājuma rezultāts:", a * b); 
+console.log("Dalījuma rezultāts:", a / b);    
 
-case"!"
-  result =1
-   for(let i =1;i< a ; i++)
-   {
-    //result = result *i;
-    result *= i;
-   }
-   break; 
+console.log("a pakāpē b:", Math.pow(a, b));     
+console.log("Dalījuma atlikums:", a % b);       
+if (a > b) {
+    console.log("a ir lielāks par b");
+} else if (a == b) {
+    console.log("a ir vienāds ar b");
+} else if (b < a) {
+    console.log("b ir mazāks par a");
+}
+switch (true) {
+    case (a > b):
+        console.log("a ir lielāks par b");
+        break;
+    case (a == b):
+        console.log("a ir vienāds ar b");
+        break;
+    case (b < a):
+        console.log("b ir mazāks par a");
+        break;
+}
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-   default:
-    console.log(result:)
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
 }
 
-result = a + b;
-console.log(result: ",result;)     
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for (let i = array.length - 1; i >= 0; i--) {
+    console.log(array[i]);
+}
